da993ee020a2b3074a12c6508c0032c873bb2ca3
OscarMaldonado <oscarj23@uw.edu>
2024-04-14 19:48:33 -0700

    info cards, news and public health screens rough prototype

--- components/screens/News.js
+++ components/screens/News.js
@@ -1,14 +1,32 @@
 import * as React from 'react';
-import { View, Text, Alert} from 'react-native';
+import { View, Text, Alert, ScrollView, StyleSheet} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import InfoItem from '../InfoItem';
 
 export default function NewsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <ScrollView contentContainerStyle={styles.container}>
       <StatusBar style='auto' />
-      <Text onPress={() => Alert.alert("You pressed the news button!")}
-        style={{ fontSize: 26, fontWeight: 'bold'}}> News Screen
-      </Text>
-    </View>
+      <Text style={styles.text}> Recent News</Text>
+      <InfoItem description={"A Brief History of the Confederated Tribes of the Colville Reservation"}/>
+      <InfoItem description={"Colville Cultural Information"}/>
+      <Text style={styles.text}> Past Events</Text>
+      <InfoItem description={"Winter Pow wow"}/>
+      <InfoItem description={"Food Drive on 5th"}/>
+    </ScrollView>
 	);
 }
+
+const styles = StyleSheet.create({
+	container : {
+		justifyContent: 'center',
+    padding: 20
+	},
+  text: {
+    fontSize: 26, 
+    padding: 5,
+    fontWeight: 'bold', 
+    color: 'rgba(5, 16, 101, 1)',
+    alignContent: 'flex-start'
+  }
+})
